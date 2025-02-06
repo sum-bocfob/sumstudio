1e5a0ac867470c3d53b51d73dadb8de655a87b57
sum-bocfob <43840414+sum-bocfob@users.noreply.github.com>
2025-02-07 00:19:04 +0900

    base with if else

--- baseOfRelPath.js
+++ baseOfRelPath.js
@@ -1,6 +1,19 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     var baseTag = document.createElement("base");
+//     baseTag.href = "https://sum-bocfob.github.io/sumstudio/";
+//     // baseTag.href = "/";
+//     document.head.appendChild(baseTag);
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
     var baseTag = document.createElement("base");
-    baseTag.href = "https://sum-bocfob.github.io/sumstudio/";
-    // baseTag.href = "/";
+
+    // 環境による判定
+    if (window.location.hostname === "localhost") {
+        baseTag.href = "/"; // ローカル環境の場合
+    } else {
+        baseTag.href = "https://sum-bocfob.github.io/sumstudio/"; // GitHub Pages の URL
+    }
+
     document.head.appendChild(baseTag);
 });
