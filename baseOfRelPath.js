ff941f11e8322e874ff29a0f9f4ef12a279e1c81
sum-bocfob <43840414+sum-bocfob@users.noreply.github.com>
2025-02-06 23:16:19 +0900

    use base tag

---
+++ baseOfRelPath.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var baseTag = document.createElement("base");
+    baseTag.href = "https://sum-bocfob.github.io/sumstudio/";
+    // baseTag.href = "/";
+    document.head.appendChild(baseTag);
+});
