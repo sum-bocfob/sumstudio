cf27fa809db2ce2e71b6d431d09cf918a2068660
sum-bocfob <43840414+sum-bocfob@users.noreply.github.com>
2025-02-06 23:45:26 +0900

    base url change

--- baseOfRelPath.js
+++ baseOfRelPath.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     var baseTag = document.createElement("base");
-    baseTag.href = "https://sum-bocfob.github.io/sumstudio/";
+    baseTag.href = "https://github.com/sum-bocfob/sumstudio/blob/main/";
     // baseTag.href = "/";
     document.head.appendChild(baseTag);
 });
