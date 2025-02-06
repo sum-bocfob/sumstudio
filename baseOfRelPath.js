317694f8c012ded66dce7b7a3df3f2917479c7d7
sum-bocfob <43840414+sum-bocfob@users.noreply.github.com>
2025-02-06 23:47:25 +0900

    baseeeee

--- baseOfRelPath.js
+++ baseOfRelPath.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     var baseTag = document.createElement("base");
-    baseTag.href = "https://github.com/sum-bocfob/sumstudio/blob/main/";
+    baseTag.href = "https://github.com/sum-bocfob/sumstudio/";
     // baseTag.href = "/";
     document.head.appendChild(baseTag);
 });
