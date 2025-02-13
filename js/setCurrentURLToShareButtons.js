1eaefb991218ca51304bb00cfa0c9fbfd4ed3fa2
sum-bocfob <43840414+sum-bocfob@users.noreply.github.com>
2025-02-13 12:27:35 +0900

    fix JP share

--- js/setCurrentURLToShareButtons.js
+++ js/setCurrentURLToShareButtons.js
@@ -1,40 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // 現在のURLをエンコード（URLエンコード）
+function setCurrentURLToShareButtons() {
     var currentUrl = encodeURIComponent(window.location.href);
+    var mainUrl = window.location.href.replace(window.location.protocol + "//", "");
 
-    // Twitter
-    // シェアURLを作成
-    var shareUrl = "https://twitter.com/share?url=" + currentUrl + "&hashtags=blender,blender3d,b3d,blenderaddon";
-    // クラスを持つリンクのhref属性を更新
-    var snsBtn = document.querySelector("share_twitter");
-    if (snsBtn) {
-        snsBtn.href = shareUrl;
+    // TwitterのシェアURLを生成
+    var twitterShareUrl = "https://twitter.com/share?url=" + currentUrl;
+    var twitterBtn = document.querySelector(".share_twitter");
+    if (twitterBtn) {
+        twitterBtn.href = twitterShareUrl;
+    } else {
+        console.error("Twitter share button not found.");
     }
 
-    // Facebook
-    // シェアURLを作成
-    var shareUrl = "https://www.facebook.com/sharer/sharer.php?u=" + currentUrl;
-    // クラスを持つリンクのhref属性を更新
-    var snsBtn = document.querySelector("share_facebook");
-    if (snsBtn) {
-        snsBtn.href = shareUrl;
+    // FacebookのシェアURLを生成
+    var facebookShareUrl = "https://www.facebook.com/sharer/sharer.php?u=" + currentUrl;
+    var facebookBtn = document.querySelector(".share_facebook");
+    if (facebookBtn) {
+        facebookBtn.href = facebookShareUrl;
+    } else {
+        console.error("Facebook share button not found.");
     }
 
-    // LINE
-    // シェアURLを作成
-    var shareUrl = "https://social-plugins.line.me/lineit/share?url=" + currentUrl;
-    // クラスを持つリンクのhref属性を更新
-    var snsBtn = document.querySelector("share_line");
-    if (snsBtn) {
-        snsBtn.href = shareUrl;
+    // LINEのシェアURLを生成
+    var lineShareUrl = "https://social-plugins.line.me/lineit/share?url=" + currentUrl;
+    var lineBtn = document.querySelector(".share_line");
+    if (lineBtn) {
+        lineBtn.href = lineShareUrl;
+    } else {
+        console.error("LINE share button not found.");
     }
 
-    // はてなブックマーク
-    // シェアURLを作成
-    var shareUrl = "https://b.hatena.ne.jp/entry/s/" + currentUrl;
-    // クラスを持つリンクのhref属性を更新
-    var snsBtn = document.querySelector("share_hatena");
-    if (snsBtn) {
-        snsBtn.href = shareUrl;
+    // はてなブックマークのシェアURLを生成
+    console.log(currentUrl);
+    console.log(mainUrl);
+    var hatenaShareUrl = "https://b.hatena.ne.jp/entry/s/" + mainUrl;
+    var hatenaBtn = document.querySelector(".share_hatena");
+    if (hatenaBtn) {
+        hatenaBtn.href = hatenaShareUrl;
+    } else {
+        console.error("Hatena bookmark share button not found.");
     }
-});
+}
